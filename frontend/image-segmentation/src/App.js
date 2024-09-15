bf0f9e0b42619de0d26d281226799dafc2fa5ca6
naangergely <naangergely@gmail.com>
2024-09-15 19:39:17 +0200

    Basic_UI
    React_frontend_base_implementation
    I_chose_MaterialUI_for_component

---
+++ frontend/image-segmentation/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import "@radix-ui/themes/styles.css";
+import ScreenSelector from './components/SelectorScreen.tsx';
+
+function App() {
+  return (
+    <div>
+      <ScreenSelector />
+    </div>
+  );
+}
+
+export default App;
