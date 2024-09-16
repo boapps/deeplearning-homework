746c385d64859dca207550e54ae89ae29dcd931b
boa <boa@protonmail.com>
2024-09-16 15:50:14 +0200

    improve conversion

--- convert_dataset.py
+++ convert_dataset.py
@@ -2,6 +2,7 @@ import os
 import json
 import xmltodict
 from tqdm import tqdm
+import pandas as pd
 
 def order_dict(dictionary):
     result = {}
@@ -11,6 +12,18 @@ def order_dict(dictionary):
         else:
             result[k] = v
     return result
+
+def process_object(object):
+    df = pd.json_normalize([object], sep='_')
+    # flatten objects
+    object = order_dict(df.to_dict(orient='records')[0])
+    # fix types
+    object['bndbox_xmax'] = int(object['bndbox_xmax'])
+    object['bndbox_ymax'] = int(object['bndbox_ymax'])
+    object['bndbox_xmin'] = int(object['bndbox_xmin'])
+    object['bndbox_ymin'] = int(object['bndbox_ymin'])
+
+    return object
 
 def convert_xml_to_json_lines(input_folder, output_file):
     with open(output_file, 'w') as jsonl_file:
@@ -22,7 +35,19 @@ def convert_xml_to_json_lines(input_folder, output_file):
                     try:
                         # Convert XML to dictionary
                         xml_dict = order_dict(xmltodict.parse(xml_file.read())['annotation'])
-                        
+                        # force objects to be a list
+                        xml_dict['object'] = xml_dict['object'] if type(xml_dict['object']) == list else [xml_dict['object']]
+                        # flatten objects
+                        df = pd.json_normalize([xml_dict], sep='_')
+                        xml_dict = df.to_dict(orient='records')[0]
+                        # fix types
+                        xml_dict['segmented'] = True if xml_dict['segmented'] == '1' else False
+                        xml_dict['size_depth'] = int(xml_dict['size_depth'])
+                        xml_dict['size_height'] = int(xml_dict['size_height'])
+                        xml_dict['size_width'] = int(xml_dict['size_width'])
+                        # fix objects
+                        xml_dict['object'] = [process_object(object) for object in xml_dict['object']]
+
                         # Convert dictionary to JSON string, and write to output file
                         json_line = json.dumps(xml_dict)
                         jsonl_file.write(json_line + '\n')
