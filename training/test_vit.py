c0de90ea9e1a127190f5113fac0d008c48ad86dc
naangergely <naangergely@gmail.com>
2024-11-05 19:14:09 +0100

    import nn

--- training/test_vit.py
+++ training/test_vit.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import numpy as np
 from datasets import Dataset, Image
 from transformers import AutoModelForSemanticSegmentation, AutoImageProcessor, TrainingArguments, Trainer
